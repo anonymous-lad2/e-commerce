2dcbb753c3873b5086bff3f24b0485e13aa22c87
Pablo Rodriguez Sanz <pablo.rodrguezsanz@yahoo.com>
2025-04-03 19:33:58 +0800

    Updated settings.py, grouped apps together in my apps section, for helping keep track of them as project grows

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -31,15 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps.
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'store',
+    # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework_simplejwt',
-    'store',
 ]
 
 MIDDLEWARE = [
