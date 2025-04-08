8bf062072b56bc410a2bcaef1b32bac2d008616f
anonymous-lad2 <anonymouslad540@gmail.com>
2025-04-08 08:57:28 +0530

    react-tailwind setup done

---
+++ front_end/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-500">
+      <h1 className="text-3xl font-extrabold font-serif">Ecommerce App</h1>
+    </div>
+  );
+}
+
+export default App;
